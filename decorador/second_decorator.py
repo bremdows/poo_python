e5db1df2640dc85e163b29f10fc6a5727a5d03ec
bremdows <bremdows@gmail.com>
2022-04-03 16:58:17 -0500

    Funciones creadas para crear los decoradores

--- decorador/second_decorator.py
+++ decorador/second_decorator.py
@@ -4,4 +4,20 @@
     Making an ice cream without choco chips
 """
 
-def
+def add_choco_chips( param_function ) :
+    def internal_function() :
+        print("Adding choco chips")
+        param_function()
+        print("Ice cream with choco chips")
+    return internal_function
+
+
+def vanilla_icre_cream() :
+    return "Vanilla icre cream"
+
+def lucuma_ice_cream() :
+    return "Lucuma ice cream"
+
+print( vanilla_icre_cream() )
+
+print( lucuma_ice_cream() )
